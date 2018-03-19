d3914d53aaadf0d2042588c4b1b5725e687b4d86
Matt <matt@ideajunction.uk>
2018-03-19 21:37:44 +0000

    Added coinlist components

---
+++ components/Coins/Coin.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-flex
+    xs12
+    sm6
+    md4
+    lg3>
+
+    <v-card>
+      <v-card-title><h4>{{ item.name }}</h4></v-card-title>
+      <v-divider></v-divider>
+      <v-list dense>
+        <v-list-tile>
+          <v-list-tile-content>Price:</v-list-tile-content>
+          <v-list-tile-content class="align-end">${{ parseFloat(item.price_usd).toFixed(2) }}</v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile>
+          <v-list-tile-content>24hr Volume:</v-list-tile-content>
+          <v-list-tile-content class="align-end">${{ parseFloat(item['24h_volume_usd']).toFixed(2) }}</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-card>
+
+  </v-flex>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
---
+++ components/Coins/CoinList.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container fluid grid-list-sm>
+    <v-data-iterator
+      content-tag="v-layout"
+      row
+      wrap
+      hide-actions
+      :items="items">
+
+      <Coin slot="item" slot-scope="props" :item="props.item" />
+
+    </v-data-iterator>
+  </v-container>
+</template>
+
+<script>
+import Coin from './Coin'
+
+export default {
+  props: {
+    items: {
+      type: Array,
+      required: true
+    }
+  },
+  components: {
+    Coin
+  }
+}
+</script>
